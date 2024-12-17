dcd2459f0dd9d8e8b723b769983a9f1bf8540cd8
ThuanNgcodelor <nguyentrungthuan417@gmail.com>
2024-12-17 16:16:28 +0700

    haha

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb+srv://davidnguyen:thuan417@davidnguyen.7rlnm.mongodb.net/?retryWrites=true&w=majority&appName=Davidnguyen');
+        await mongoose.connect('mongodb+srv://davidnguyen:thuan417@davidnguyen.7rlnm.mongodb.net/?retryWrites=true&w=majority&appName=NodeJs');
         console.log('Ok');
     } catch (err) {
         console.log('Not found', err);
