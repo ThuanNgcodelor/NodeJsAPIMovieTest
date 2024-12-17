074266408fcbb187a6351a6ae0dacd7b6dab27f1
ThuanNgcodelor <nguyentrungthuan417@gmail.com>
2024-12-17 16:25:18 +0700

    huhu

--- config/config.js
+++ config/config.js
@@ -2,10 +2,11 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb+srv://davidnguyen:thuan417@davidnguyen.7rlnm.mongodb.net/?retryWrites=true&w=majority&appName=NodeJs');
+        await mongoose.connect('mongodb+srv://davidnguyen:thuan417@davidnguyen.7rlnm.mongodb.net/?retryWrites=true&w=majority&appName=Davidnguyen');
+        // await mongoose.connect('mongodb+srv://davidnguyen:thuan417@davidnguyen.7rlnm.mongodb.net/?retryWrites=true&w=majority&appName=NodeJs');
         console.log('Ok');
     } catch (err) {
-        console.log('Not found', err);
+        console.log('Hentai', err);
         process.exit(1);
     }
 };
